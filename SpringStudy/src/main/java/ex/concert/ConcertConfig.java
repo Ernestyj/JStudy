799d9cf95a7e1bd3fa4319ec19ba666b221cb370
ernestyj@outlook.com <ernestyj@outlook.com>
2015-09-29 20:06:49 +0800

    add ex.concert

---
+++ SpringStudy/src/main/java/ex/concert/ConcertConfig.java
@@ -0,0 +1,19 @@
+package ex.concert;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Created by Jian on 2015/9/29.
+ */
+@Configuration
+@ComponentScan
+@EnableAspectJAutoProxy
+public class ConcertConfig {
+    @Bean
+    public Audience audience(){
+        return new Audience();
+    }
+}
---
+++ SpringStudy/src/main/java/ex/concert/Performance.java
@@ -0,0 +1,8 @@
+package ex.concert;
+
+/**
+ * Created by Jian on 2015/9/29.
+ */
+public interface Performance {
+    void perform();
+}
