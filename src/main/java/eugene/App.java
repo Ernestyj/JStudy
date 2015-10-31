f9d9e7127976dcb4540570fd70343baa50351d1e
ernestyj <ernestyj@outlook.com>
2015-10-31 15:30:30 +0800

    clean project

--- src/main/java/eugene/App.java
+++ src/main/java/eugene/App.java
@@ -1,16 +1,31 @@
 package eugene;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 /**
  * Created by Jian on 2015/7/28.
  */
 public class App {
     public static void main(String[] args){
         System.out.println("*****RESULT*****");
-        test();
+
+        try {
+            test();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    private static void test() {
+    private static void test() throws IOException {
+
+    }
+
+    private static void test(final String inputString) throws IOException {
     }
 
 
