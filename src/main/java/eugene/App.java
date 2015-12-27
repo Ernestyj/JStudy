9671414545c49b62bad0bf0442b4a46382b0b48d
ernestyj <ernestyj@outlook.com>
2015-12-27 11:49:56 +0800

    new algos

--- src/main/java/eugene/App.java
+++ src/main/java/eugene/App.java
@@ -11,42 +11,8 @@ public class App {
     public static void main(String[] args){
         System.out.println("*****RESULT*****");
 
-
-        System.out.println();
-//        int h = 0x10000000;
-//        System.out.println(Integer.toBinaryString(h));
-//        for(int i = 0; i < 28; i++) {
-//            h >>>= 1;
-//            System.out.println(Integer.toBinaryString(h));
-//        }
-//        int h = -1;
-//        System.out.println(Integer.toBinaryString(h));
-//        h <<= 10;
-//        System.out.println(Integer.toBinaryString(h));
-//        for(int i = 0; i < 32; i++) {
-//            h >>>= 1;
-//            System.out.println(Integer.toBinaryString(h));
-//        }
-        char c = 'a';
-        System.out.println(Integer.toBinaryString(c));
-        c = 'b';
-        System.out.println(Integer.toBinaryString(c));
-        c = 'c';
-        System.out.println(Integer.toBinaryString(c));
-        c = 'd';
-        System.out.println(Integer.toBinaryString(c));
-        c += 1;
-        System.out.println(Integer.toBinaryString(c));
-        c = 'A';
-        System.out.println(Integer.toBinaryString(c));
-        for(int i = 0; i < 26; i++) {
-            c +=1;
-            System.out.println(Integer.toBinaryString(c));
-        }
         try {
             test();
-//            int[] nums = {1, 0, -1, 0, -2, 2, 9};
-//            for (int n : nums) System.out.print(n + " ");
         } catch (IOException e) {
             e.printStackTrace(); 
         }
--- src/main/java/eugene/TheApp.java
+++ src/main/java/eugene/TheApp.java
@@ -12,7 +12,6 @@ public class TheApp {
     public static void main(String[] args){
         System.out.println("*****RESULT*****");
 
-
     }
 
 
