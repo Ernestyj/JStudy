187fbac088c890a711c63d8a1af67ff0a22ac2ec
Eugene <ernestyj@outlook.com>
2016-01-20 21:06:25 +0800

    new ago

--- src/main/java/eugene/App.java
+++ src/main/java/eugene/App.java
@@ -11,21 +11,13 @@ public class App {
     public static void main(String[] args){
         System.out.println("*****RESULT*****");
 
-        System.out.println(secondsToFormatTime(3600));
+        int i = (int)Integer.MIN_VALUE;
+        int j = (int)Integer.MIN_VALUE;
+        System.out.print(i == j);
+
     }
 
 
-    public static String secondsToFormatTime(int seconds){
-        int hour = seconds / 3600;
-        int minute = seconds%3600 / 60;
-        int second = seconds % 60;
-        StringBuilder builder = new StringBuilder();
-        String h = (hour/10 == 0) ? "0"+hour : String.valueOf(hour);
-        String m = (minute/10 == 0) ? "0"+minute : String.valueOf(minute);
-        String s = (second/10 == 0) ? "0"+second : String.valueOf(second);
-        builder.append(h + ":" + m + ":" + s);
-        return builder.toString();
-    }
 
 
 
