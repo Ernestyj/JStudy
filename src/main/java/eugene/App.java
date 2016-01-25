e93b92cd45b0a0b7d1e2680fcf5d9445d6f21010
Eugene <ernestyj@outlook.com>
2016-01-26 00:34:51 +0800

    Construct Binary Tree from Preorder and Inorder Traversal

--- src/main/java/eugene/App.java
+++ src/main/java/eugene/App.java
@@ -11,9 +11,11 @@ public class App {
     public static void main(String[] args){
         System.out.println("*****RESULT*****");
 
-        int i = (int)Integer.MIN_VALUE;
-        int j = (int)Integer.MIN_VALUE;
-        System.out.print(i == j);
+        int i = 0;
+        for (; i<10; i++){
+            if (i == 6) break;
+        }
+        System.out.println(i);
 
     }
 
