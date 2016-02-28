3f16d5ac4acd384f13dcafadf37171d503b9ddce
Eugene <ernestyj@outlook.com>
2016-02-28 15:06:05 +0800

    Word Break II

--- src/main/java/eugene/App.java
+++ src/main/java/eugene/App.java
@@ -11,17 +11,9 @@ public class App {
     public static void main(String[] args){
         System.out.println("*****RESULT*****");
 
-        int[] nums = {14, 14, 14, 9};
-        int result = 0;
-        for (int i=31; i>=0; i--){
-            int sum = 0;
-            int mask = 1<<i;
-            System.out.print(i + ": ");
-            for (int j=0; j<nums.length; j++){
-                System.out.print((nums[j] & mask) + " ");
-            }
-            System.out.println();
-        }
+        boolean[] canSentence = new boolean[3];
+        for (boolean b : canSentence) b=true;
+        for (boolean b : canSentence) System.out.println(b);
     }
 
 
