dd9957607b8c0172a16b632d4f0cf97ca5e9f3d0
Eugene <ernestyj@outlook.com>
2016-03-04 18:58:26 +0800

    Binary Tree Postorder Traversal

--- src/main/java/datastructure/tree/BinaryTreeNode.java
+++ src/main/java/datastructure/tree/BinaryTreeNode.java
@@ -117,6 +117,51 @@ public class BinaryTreeNode {
         visit(root);
     }
 
+    /**
+     * 后序遍历的情况相对复杂,需要维护当前遍历的cur指针和前一个遍历的pre指针来追溯当前的情况
+     （注意这里是遍历的指针，并不是真正按后序访问顺序的结点）。具体分为几种情况：
+     （1）如果pre的左孩子或者右孩子是cur，那么说明遍历在往下走，按访问顺序继续，
+     即如果有左孩子，则是左孩子进栈，否则如果有右孩子，则是右孩子进栈，
+     如果左右孩子都没有，则说明该结点是叶子，可以直接访问并把结点出栈了。
+     （2）如果反过来，cur的左孩子是pre，则说明已经在回溯往上走了，
+     但是后序遍历要左右孩子走完才可以访问自己，所以这里如果有右孩子还需要把右孩子进栈，
+     否则说明已经到自己了，可以访问并且出栈了。
+     （3）如果cur的右孩子是pre，那么说明左右孩子都访问结束了，可以轮到自己了，访问并且出栈即可。
+     与递归法一样,算法时间复杂度也是O(n)，空间复杂度是栈的大小O(logn)。
+     * http://www.programcreek.com/2012/12/leetcode-solution-of-iterative-binary-tree-postorder-traversal-in-java/
+     * @param root
+     * @return
+     */
+    public List<String> traversePostOderIterative(BinaryTreeNode root) {
+        List<String> result = new ArrayList<>();
+        if (root==null) return result;
+        Stack<BinaryTreeNode> stack = new Stack<>();
+        stack.push(root);
+        BinaryTreeNode prev = null;
+        while (!stack.isEmpty()){
+            BinaryTreeNode curr = stack.peek();
+            if (prev==null || prev.left==curr || prev.right==curr){ //往下走
+                if (curr.left!=null) stack.push(curr.left);
+                else if (curr.right!=null) stack.push(curr.right);
+                else {
+                    stack.pop();
+                    result.add(curr.val);
+                }
+            } else if (curr.left==prev){    //从左节点往上走
+                if (curr.right!=null) stack.push(curr.right);
+                else {
+                    stack.pop();
+                    result.add(curr.val);
+                }
+            } else if (curr.right==prev){   //从右节点往上走
+                stack.pop();
+                result.add(curr.val);
+            }
+            prev = curr;
+        }
+        return result;
+    }
+
     public void traverseLayer(){
         traverseLayer(this);
     }
