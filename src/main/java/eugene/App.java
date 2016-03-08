f16a44c402b6c4b422916206598787dadd335c70
Eugene <ernestyj@outlook.com>
2016-03-08 17:56:14 +0800

    Max Points on a Line

--- src/main/java/eugene/App.java
+++ src/main/java/eugene/App.java
@@ -11,9 +11,12 @@ public class App {
     public static void main(String[] args){
         System.out.println("*****RESULT*****");
 
-        boolean[] canSentence = new boolean[3];
-        for (boolean b : canSentence) b=true;
-        for (boolean b : canSentence) System.out.println(b);
+        Double x = 1.0*(1-1)/3;
+        Double y = 1.0*(5-5)/9999;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(x.equals(y));
+
     }
 
 
