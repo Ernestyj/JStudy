c10b31dcc6f3ad3c472660bfec184bf5a6d321cb
Eugene <ernestyj@outlook.com>
2016-03-17 13:42:21 +0800

    Compare Version Numbers

--- src/main/java/eugene/App.java
+++ src/main/java/eugene/App.java
@@ -11,11 +11,7 @@ public class App {
     public static void main(String[] args){
         System.out.println("*****RESULT*****");
 
-        Double x = 1.0*(1-1)/3;
-        Double y = 1.0*(5-5)/9999;
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(x.equals(y));
+        System.out.println("1".split("\\.")[0]);
 
     }
 
