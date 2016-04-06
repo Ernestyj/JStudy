bb5069a3d2fc9c8185b79516df3a1277797a22d6
Eugene <ernestyj@outlook.com>
2016-04-06 15:30:07 +0800

    Course Schedule II

--- src/main/java/eugene/App.java
+++ src/main/java/eugene/App.java
@@ -7,11 +7,23 @@ import java.util.*;
  * Created by Jian on 2015/7/28.
  */
 public class App {
+    private static final String MESSAGE="taobao";
 
     public static void main(String[] args){
         System.out.println("*****RESULT*****");
 
-        System.out.println(Integer.valueOf('1'));
+        String a ="tao"+"bao";
+        String b="tao";
+        String c="bao";
+        System.out.println(a==MESSAGE);
+        System.out.println((b+c)==MESSAGE);
+
+        Integer i = 42;
+        Long l = 42l;
+        Double d = 42.0;
+        System.out.println(i.equals(l));
+
+
     }
     private void test(){
         System.out.println(super.getClass().getSuperclass());
