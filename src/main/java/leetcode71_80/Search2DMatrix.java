6e1671964d3ab3c38f4e8eb25677a8cab430f48d
Eugene <ernestyj@outlook.com>
2016-05-04 19:44:17 +0800

    search 2d  matrix

--- src/main/java/leetcode71_80/Search2DMatrix.java
+++ src/main/java/leetcode71_80/Search2DMatrix.java
@@ -17,8 +17,35 @@ public class Search2DMatrix {
         System.out.println(new Search2DMatrix().searchMatrix(matrix, 20));
     }
 
-    //最外层也使用二分搜索
+    //将矩阵看成数组
     public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int l = 0, r = m*n-1;
+        while (l<=r) {
+            int mid = (l+r)/2;
+            if (matrix[mid/n][mid%n] == target)  return true;
+            if (matrix[mid/n][mid%n] < target)  l = mid+1;
+            else r = mid-1;
+        }
+        return false;
+    }
+
+    //与Search2DMatrixII思路一致,从右上角开始搜
+    public boolean searchMatrix0(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int i = 0, j = n-1;
+        while (i<m && j>=0){
+            if (matrix[i][j]==target){
+                return true;
+            }else if (matrix[i][j]<target) i++;
+            else j--;
+        }
+        return false;
+    }
+
+
+    //最外层也使用二分搜索
+    public boolean searchMatrix1(int[][] matrix, int target) {
         int row = matrix.length;
         int col = matrix[0].length;
         if (target < matrix[0][0] || target > matrix[row-1][col-1]) return false;
@@ -57,7 +84,7 @@ public class Search2DMatrix {
      * @param target
      * @return
      */
-    public boolean searchMatrix1(int[][] matrix, int target) {
+    public boolean searchMatrix2(int[][] matrix, int target) {
         int row = matrix.length;
         int col = matrix[0].length;
         if (target < matrix[0][0] || target > matrix[row-1][col-1]) return false;
