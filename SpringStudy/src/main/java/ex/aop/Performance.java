7f2276802f7d17b91495875c9cb749b68c5ab1bb
V_ <v_yangjian02@internal.baidu.com>
2016-08-08 10:39:14 +0800

    更新整个项目结构

--- SpringStudy/src/main/java/ex/aop/Performance.java
+++
@@ -1,8 +0,0 @@
-package ex.aop;
-
-/**
- * Created by Jian on 2015/9/29.
- */
-public interface Performance {
-    void perform();
-}
--- SpringStudy/src/main/java/ex/ioc/MediaPlayer.java
+++
@@ -1,8 +0,0 @@
-package ex.ioc;
-
-/**
- * Created by Jian on 2015/9/28.
- */
-public interface MediaPlayer {
-    void play();
-}
