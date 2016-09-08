e7646576d744fd0df6e973534f5e09f10416b4d8
V_ <v_yangjian02@internal.baidu.com>
2016-09-08 18:57:04 +0800

    c

--- src/main/java/eugene/App.java
+++ src/main/java/eugene/App.java
@@ -5,7 +5,9 @@ import javafx.beans.binding.ObjectExpression;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.math.BigDecimal;
 import java.nio.charset.Charset;
+import java.text.NumberFormat;
 import java.util.*;
 
 /**Implement atoi to convert a string to an integer.
@@ -34,8 +36,9 @@ public class App {
         queue.add(null);
 
         System.out.println(" ".split(" ").length);
-        System.out.println(Integer.MAX_VALUE);
-
+        System.out.println(Double.compare(1.61-1.6, 0.01));
+        System.out.println(new BigDecimal("1.61").subtract(new BigDecimal("1.6")).toString());
+        System.out.println(new Object());
     }
 
     private static void print(Object... args) {
