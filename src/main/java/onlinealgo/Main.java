412d803efdf471f8af63c9d5b3bd1f14094d01b5
Eugene <ernestyj@outlook.com>
2016-09-08 22:03:01 +0800

    Didi online algos

--- src/main/java/onlinealgo/Main.java
+++ src/main/java/onlinealgo/Main.java
@@ -3,7 +3,8 @@ package onlinealgo;
 /**
  * Created by eugene on 16/8/2.
  */
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -12,5 +13,72 @@ public class Main {
             int b = in.nextInt();
             System.out.println(a + b);
         }
+    }
+
+    public static void main2(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+        int[] sizes = new int[n];
+        LinkedList<Guest> guests = new LinkedList<>();
+        for(int i=0; i<n; i++) sizes[i] = sc.nextInt();
+        for(int i=0; i<m; i++) {
+            guests.add(new Guest(sc.nextInt(), sc.nextInt()));
+        }
+        long res = 0;
+        System.out.println(res);
+    }
+    static class Guest{
+        int spend, people;
+        public Guest(int people, int spend) {
+            this.spend = spend;
+            this.people = people;
+        }
+    }
+    public static long maxIncome(int n, int m, int[] sizes, LinkedList<Guest> guests){
+        if (n==0 || m==0) return 0;
+        Collections.sort(guests, new Comparator<Guest>() {
+            @Override
+            public int compare(Guest o1, Guest o2) {
+                return Integer.compare(o2.spend, o1.spend);//降序
+            }
+        });
+        Arrays.sort(sizes);//升序
+        long res = 0;
+        ListIterator<Guest> iterator = null;
+        Guest guest = null;
+        for (int size: sizes){
+            iterator = guests.listIterator();
+            while (iterator.hasNext()) {
+                guest = iterator.next();
+                if (guest.people<=size) {
+                    res += guest.spend;
+                    iterator.remove();
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main1(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+        int res = maxSubArray(nums);
+        System.out.println(res);
+    }
+    public static int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int len = nums.length;
+        int local = nums[0];
+        int global = nums[0];
+        for (int k = 1; k < len; k++){
+            local = Math.max(local+nums[k], nums[k]);
+            global = Math.max(local, global);
+        }
+        return global;
     }
 }
