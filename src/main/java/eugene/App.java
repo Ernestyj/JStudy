0a4fdd93ffe8a7a7b0e74c8f5a6c43be42de7b7b
Jian Yang <ernestyj@outlook.com>
2016-10-15 13:00:33 +0800

    u

--- src/main/java/eugene/App.java
+++ src/main/java/eugene/App.java
@@ -1,17 +1,8 @@
 package eugene;
 
-import javafx.beans.binding.ObjectExpression;
-
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.math.BigDecimal;
-import java.nio.charset.Charset;
-import java.text.NumberFormat;
-import java.util.*;
 
-/**Implement atoi to convert a string to an integer.
- Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
+/**
  * Created by Jian on 2015/7/28.
  */
 public class App {
@@ -21,24 +12,15 @@ public class App {
         double x = 0.0;
         System.out.println(Math.abs(x)<0.00001);
 
-        print(new int[]{1,2,3});
-        f(1, 'C');
-        //f('C','a'); 报错对f的引用不明确
-
-//        ArrayList<String> list = new ArrayList<>();
-//        Iterator<String> iterator = list.listIterator();
-//        java.util.Collections collections;
-        HashMap<String, String> map;
-        LinkedList<String> queue = new LinkedList<>();
-//        queue = Collections.unmodifiableCollection();
-        ArrayDeque q = new ArrayDeque();
-        //q.add(null);
-        queue.add(null);
+        print(new int[]{1, 2, 3});
 
         System.out.println(" ".split(" ").length);
+
         System.out.println(Double.compare(1.61-1.6, 0.01));
         System.out.println(new BigDecimal("1.61").subtract(new BigDecimal("1.6")).toString());
-        System.out.println(new Object());
+
+        System.out.println(Math.round(12F));//float 返回int, double 返回long
+
     }
 
     private static void print(Object... args) {
@@ -46,14 +28,4 @@ public class App {
         System.out.println();
     }
 
-    static void f(float a, Character... args){
-        System.out.println(a);
-    }
-    static void f(char c, Character... args){
-        System.out.println(c);
-    }
-    static void f(Character... args){ }
-
-
 }
-class A extends Object{}
